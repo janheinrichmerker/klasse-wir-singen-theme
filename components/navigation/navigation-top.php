21625e3826f7c0807347d8fb64f3a23de770f9e4
Jan Heinrich Reimer <heinrich@reimer.family>
2021-08-04 18:18:48 +0200

    Improve menu styles

--- components/navigation/navigation-top.php
+++ components/navigation/navigation-top.php
@@ -1,4 +1,4 @@
 <nav id="site-navigation" class="main-navigation" role="navigation">
-	<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'dara' ); ?></button>
+	<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false"><?php esc_html_e( 'Menü', 'dara' ); ?></button>
 	<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'top-menu' ) ); ?>
 </nav>
