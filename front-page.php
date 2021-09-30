8325240472362ef68d54b6aedc3be0de1919fbea
Jan Heinrich Reimer <heinrich@reimer.family>
2021-09-30 15:13:24 +0200

    Update theme, hard-code front page

--- front-page.php
+++ front-page.php
@@ -33,7 +33,70 @@ get_header(); ?>
 	</div><!-- #primary -->
 </div><!-- .content-wrapper -->
 
-<?php dara_featured_pages(); ?>
+<?php //dara_featured_pages(); ?>
+
+<div id="quaternary" class="featured-page-area">
+    <div class="featured-page-wrapper clear">
+        <div class="featured-page">
+            <article class="page type-page status-publish has-post-thumbnail hentry tag-jubilaeum with-featured-image">
+<!--                <header class="entry-header">-->
+<!--                    <h2 class="entry-title">-->
+<!--                        <a href="/jubilaeum/" rel="bookmark" title="75. Geburtstag">75.&nbsp;Landesgeburtstag</a>-->
+<!--                    </h2>-->
+<!--                </header>-->
+                <a href="/jubilaeum/">
+                    <img src="/wp-content/uploads/2021/09/Button-Jubilaeum.png"
+                         class="attachment-post-thumbnail size-post-thumbnail wp-post-image jetpack-lazy-image jetpack-lazy-image--handled"
+                         alt="Landesdgeburtstag"
+                         loading="eager">
+                </a>
+                <div class="entry-summary">
+                    <p>
+                        Am 1.&nbsp;November&nbsp;2021 wird das Land Niedersachsen 75. Feiern Sie mit!
+                    </p>
+                </div>
+            </article>
+        </div>
+        <div class="featured-page">
+            <article class="page type-page status-publish has-post-thumbnail hentry tag-musical with-featured-image">
+<!--                <header class="entry-header">-->
+<!--                    <h2 class="entry-title">-->
+<!--                        <a href="/musical/" rel="bookmark" title="Musical">Musical</a>-->
+<!--                    </h2>-->
+<!--                </header>-->
+                <a href="/musical/">
+                    <img src="/wp-content/uploads/2021/09/Button-Musical.png"
+                         class="attachment-post-thumbnail size-post-thumbnail wp-post-image jetpack-lazy-image jetpack-lazy-image--handled"
+                         alt="Musical"
+                         loading="eager">
+                </a>
+                <div class="entry-summary">
+                    <p>
+                        Das Kindermusical zum Mitmachen, erstmals 2022!
+                    </p>
+                </div>
+            </article>
+        </div>
+        <div class="featured-page">
+            <article class="page type-page status-publish has-post-thumbnail hentry tag-original with-featured-image">
+<!--                <header class="entry-header">-->
+<!--                    <h2 class="entry-title">-->
+<!--                        <a href="/original/" rel="bookmark" title="Original">Original</a>-->
+<!--                    </h2>-->
+<!--                </header>-->
+                <a href="/original/">
+                    <img src="/wp-content/uploads/2021/09/Button-Original.png"
+                         class="attachment-post-thumbnail size-post-thumbnail wp-post-image jetpack-lazy-image jetpack-lazy-image--handled"
+                         alt="Original"
+                         loading="eager">
+                </a>
+                <div class="entry-summary">
+                    <p>Klasse! Wir singen Liederfeste, wie Sie sie kennen und lieben.</p>
+                </div>
+            </article>
+        </div>
+    </div>
+</div>
 
 <?php get_footer();
 
