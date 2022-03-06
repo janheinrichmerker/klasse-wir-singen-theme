f2eec533ab3777539bcb5c1702f47383d4f2f65a
Jan Heinrich Reimer <heinrich@reimer.family>
2022-03-06 18:16:14 +0100

    Adjust logo based on tag

--- components/header/site-branding.php
+++ components/header/site-branding.php
@@ -1,6 +1,17 @@
 <div class="site-branding">
     <a href="/" class="logo-link" rel="home">
-        <img src="<?php echo(bloginfo('template_directory')); ?>/images/logo.svg"
+        <?php
+        $tags = get_the_tags();
+        foreach ( $tags as $tag ) {
+            if ($tag->slug == "musical") {
+                $logo_file = "logo-devil.svg";
+            }
+        }
+        if (!isset($logo_file)) {
+            $logo_file = "logo.svg";
+        }
+        ?>
+        <img src="<?php echo(bloginfo('template_directory')); ?>/images/<?php echo($logo_file) ?>"
              class="logo"
              alt="Klasse! Wir singen">
     </a>
